294dfcdf9c5103476c0ffa285c04fbb9550ced3a
Treenzzgod <gio12619@gmail.com>
2022-04-05 06:22:06 +0700

    fix

--- lib/Pages/image_view.dart
+++ lib/Pages/image_view.dart
@@ -1,165 +1,91 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-//import 'dart:js' as js;
-
 class ImageView extends StatefulWidget {
-  final String imgPath;
-
-  ImageView({@required this.imgPath});
+  final String imgUrl;
+  ImageView({required this.imgUrl});
 
   @override
-  _ImageViewState createState() => _ImageViewState();
+  State<ImageView> createState() => _ImageViewState();
 }
 
 class _ImageViewState extends State<ImageView> {
-  var filePath;
-
-  _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget>[
+        children: [
           Hero(
-            tag: widget.imgPath,
-            child: Container(
+            tag: widget.imgUrl,
+            child: SizedBox(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              child: kIsWeb
-                  ? Image.network(widget.imgPath, fit: BoxFit.cover)
-                  : CachedNetworkImage(
-                      imageUrl: widget.imgPath,
-                      placeholder: (context, url) => Container(
-                        color: Color(0xfff5f8fd),
-                      ),
-                      fit: BoxFit.cover,
-                    ),
+              child: Image.network(widget.imgUrl, fit: BoxFit.cover),
             ),
           ),
           Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             alignment: Alignment.bottomCenter,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                InkWell(
-                    onTap: () {
-                      if (kIsWeb) {
-                        _launchURL(widget.imgPath);
-                        //js.context.callMethod('downloadUrl',[widget.imgPath]);
-                        //response = await dio.download(widget.imgPath, "./xx.html");
-                      } else {
-                        _save();
-                      }
-                      //Navigator.pop(context);
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width / 2,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xff1C1B1B).withOpacity(0.8),
-                            borderRadius: BorderRadius.circular(40),
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Stack(children: [
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2,
+                    color: Color(0xff1C1B1B).withOpacity(0.8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2,
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white54,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color(0x36FFFFFF),
+                            Color(0x0FFFFFFF),
+                          ],
+                        )),
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Set Wallpaper',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white70,
                           ),
                         ),
-                        Container(
-                            width: MediaQuery.of(context).size.width / 2,
-                            height: 50,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.white24, width: 1),
-                                borderRadius: BorderRadius.circular(40),
-                                gradient: LinearGradient(
-                                    colors: [
-                                      Color(0x36FFFFFF),
-                                      Color(0x0FFFFFFF)
-                                    ],
-                                    begin: FractionalOffset.topLeft,
-                                    end: FractionalOffset.bottomRight)),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "Set Wallpaper",
-                                  style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                SizedBox(
-                                  height: 1,
-                                ),
-                                Text(
-                                  kIsWeb
-                                      ? "Image will open in new tab to download"
-                                      : "Image will be saved in gallery",
-                                  style: TextStyle(
-                                      fontSize: 8, color: Colors.white70),
-                                ),
-                              ],
-                            )),
+                        Text(
+                          'Image Will be saved in gallery',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white70,
+                          ),
+                        ),
                       ],
-                    )),
-                SizedBox(
-                  height: 16,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    "Cancel",
-                    style: TextStyle(
-                        color: Colors.white60,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                )
-              ],
-            ),
+                ]),
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Cancel',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+              const SizedBox(height: 50),
+            ]),
           )
         ],
       ),
     );
-  }
-
-  _save() async {
-    await _askPermission();
-    var response = await Dio().get(widget.imgPath,
-        options: Options(responseType: ResponseType.bytes));
-    final result =
-        await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
-    print(result);
-    Navigator.pop(context);
-  }
-
-  _askPermission() async {
-    if (Platform.isIOS) {
-      /*Map<PermissionGroup, PermissionStatus> permissions =
-          */await PermissionHandler()
-              .requestPermissions([PermissionGroup.photos]);
-    } else {
-     /* PermissionStatus permission = */await PermissionHandler()
-          .checkPermissionStatus(PermissionGroup.storage);
-    }
   }
 }
--- lib/Pages/search.dart
+++ lib/Pages/search.dart
@@ -1,100 +1,85 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:wallpaper_flutter/data/data.dart';
 import 'package:wallpaper_flutter/models/photo_model.dart';
 import 'package:wallpaper_flutter/widgets/widgets.dart';
 
-class SearchView extends StatefulWidget {
+class Search extends StatefulWidget {
   final String search;
-
-  SearchView({@required this.search});
+  Search({required this.search});
 
   @override
-  _SearchViewState createState() => _SearchViewState();
+  State<Search> createState() => _SearchState();
 }
 
-class _SearchViewState extends State<SearchView> {
-  List<PhotosModel> photos = new List();
-  TextEditingController searchController = new TextEditingController();
+class _SearchState extends State<Search> {
+  TextEditingController search = TextEditingController();
+  List<photoModel> photo = [];
 
-  getSearchWallpaper(String searchQuery) async {
-    await http.get(
-        "https://api.pexels.com/v1/search?query=$searchQuery&per_page=30&page=1",
-        headers: {"Authorization": apiKEY}).then((value) {
-      //print(value.body);
+  getSearchWallpapers(String query) async {
+    var response = await http.get(
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=15&page=1"),
+        headers: {"Authorization": apiKEY});
 
-      Map<String, dynamic> jsonData = jsonDecode(value.body);
-      jsonData["photos"].forEach((element) {
-        //print(element);
-        PhotosModel photosModel = new PhotosModel();
-        photosModel = PhotosModel.fromMap(element);
-        photos.add(photosModel);
-        //print(photosModel.toString()+ "  "+ photosModel.src.portrait);
-      });
-
-      setState(() {});
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      // print(element);
+      photoModel photosModel = new photoModel();
+      photosModel = photoModel.fromMap(element);
+      photo.add(photosModel);
     });
+
+    setState(() {});
   }
 
   @override
   void initState() {
-    getSearchWallpaper(widget.search);
-    searchController.text = widget.search;
+    getSearchWallpapers(widget.search);
     super.initState();
+    search.text = widget.search;
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: brandName(),
         elevation: 0.0,
-        actions: <Widget>[
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Icon(
-                Icons.add,
-                color: Colors.white,
-              ))
-        ],
       ),
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Container(
           child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 16,
-              ),
+            children: [
               Container(
                 decoration: BoxDecoration(
-                  color: Color(0xfff5f8fd),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                margin: EdgeInsets.symmetric(horizontal: 24),
-                padding: EdgeInsets.symmetric(horizontal: 24),
+                    color: Color(0xfff5f8fd),
+                    borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                margin: EdgeInsets.symmetric(horizontal: 24.0),
                 child: Row(
-                  children: <Widget>[
+                  children: [
                     Expanded(
-                        child: TextField(
-                      controller: searchController,
-                      decoration: InputDecoration(
-                          hintText: "search wallpapers",
-                          border: InputBorder.none),
-                    )),
-                    InkWell(
-                        onTap: () {
-                          getSearchWallpaper(searchController.text);
-                        },
-                        child: Container(child: Icon(Icons.search)))
+                      child: TextField(
+                        controller: search,
+                        decoration: InputDecoration(
+                            hintText: "Search Wallpaper",
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        getSearchWallpapers(search.text);
+                      },
+                      child: Container(child: Icon(Icons.search)),
+                    )
                   ],
                 ),
               ),
               SizedBox(
-                height: 30,
+                height: 16,
               ),
-              wallPaper(photos, context),
+              WallpapersList(wallpapers: photo, context: context),
             ],
           ),
         ),
