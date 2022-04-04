354e810abb241029a8d74e674c377aa96043ced6
Treenzzgod <gio12619@gmail.com>
2022-04-05 04:52:06 +0700

    fix model

--- lib/models/photo_model.dart
+++ lib/models/photo_model.dart
@@ -1,40 +1,35 @@
-class PhotosModel {
-  String url;
-  String photographer;
-  String photographerUrl;
-  int photographerId;
-  SrcModel src;
+class WallpaperModel{
+  String? photographer;
+  String? photographer_url;
+  int? photographer_id;
+  SrcModel? src;
 
-  PhotosModel(
-      {this.url,
-      this.photographer,
-      this.photographerId,
-      this.photographerUrl,
-      this.src});
+  WallpaperModel(
+    {this.photographer,
+    this.photographer_id,
+    this.photographer_url,
+    this.src});
 
-  factory PhotosModel.fromMap(Map<String, dynamic> parsedJson) {
-    return PhotosModel(
-        url: parsedJson["url"],
-        photographer: parsedJson["photographer"],
-        photographerId: parsedJson["photographer_id"],
-        photographerUrl: parsedJson["photographer_url"],
-        src: SrcModel.fromMap(parsedJson["src"]));
+  factory WallpaperModel.fromMap(Map<String, dynamic> jsonData) {
+    return WallpaperModel(
+        photographer: jsonData["photographer"],
+        photographer_id: jsonData["photographer_id"],
+        photographer_url: jsonData["photographer_url"],
+        src: SrcModel.fromMap(jsonData["src"]));
   }
 }
 
 class SrcModel {
-  String portrait;
-  String large;
-  String landscape;
-  String medium;
+  String? original;
+  String? small;
+  String? portrait;
 
-  SrcModel({this.portrait, this.landscape, this.large, this.medium});
+  SrcModel({this.original, this.small, this.portrait});
 
-  factory SrcModel.fromMap(Map<String, dynamic> srcJson) {
+  factory SrcModel.fromMap(Map<String, dynamic> jsonData) {
     return SrcModel(
-        portrait: srcJson["portrait"],
-        large: srcJson["large"],
-        landscape: srcJson["landscape"],
-        medium: srcJson["medium"]);
+        original: jsonData["original"],
+        small: jsonData["small"],
+        portrait: jsonData["portrait"]);
   }
 }
